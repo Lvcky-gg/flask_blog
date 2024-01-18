ac969cd15d4dcac33417f9d47acf377b48c4ba4d
lvcky-gg <matadorcrab@gmail.com>
2024-01-17 19:15:58 -0500

    solved minor bug

--- controller/routes/post_routes.py
+++ controller/routes/post_routes.py
@@ -15,7 +15,7 @@ post_routes_blueprint = Blueprint("posts", __name__)
 @post_routes_blueprint.route("/", methods=["GET"])
 def get_all_posts():
     try:
-        all_posts = Post.query().all()
+        all_posts = Post.query.all()
     except BaseException as e:
         return handle_error(e)
     if all_posts:
