eff495c8cf779751ca90f6d04d9f9c3a0cd7850a
lvcky-gg <matadorcrab@gmail.com>
2024-01-23 19:36:31 -0500

    started tokenizer

---
+++ controller/markdown_compiler/test_file.md
@@ -0,0 +1,32 @@
+<!-- headings -->
+# h1
+## h2
+### h3
+#### h4 
+##### h5
+###### h6
+<!-- bold -->
+**bold**
+<!-- italic -->
+*italic*
+<!-- lists -->
+1. one
+2. two 
+3. three
+    1. test
+    2. test
+    3. test
+- one
+- two
+- three
+    - one
+    - two
+    - three
+<!-- code block -->
+`code`
+<!-- horizontal rule -->
+---
+<!-- link -->
+[title](https://www.johnodonnell.xyz)
+<!-- image -->
+![text](image.jpg)
---
+++ controller/markdown_compiler/tokenizer.py
@@ -0,0 +1,51 @@
+
+def tokenize_main(string):
+    delimiter = ",~"
+    string_literal = split_on_delimiter(string, delimiter)
+    return seperate_to_token(string_literal)
+
+def split_on_delimiter(string, delimiter):
+    return string.split(delimiter)
+
+def seperate_to_token(str):
+    returnArr = []
+    for i in range(0, len(str)):
+        
+        if str[i][0] == "\n":
+            str[i] = str[i][1::]
+
+        
+        if str[i][0] == "#":
+            returnArr.append(handle_hashtags(str[i], 0))
+        # handle this in another function to check if first is equal to a number
+        elif str[i][0].isdigit() and str[i][0] != "!":
+            val = str[i][0]
+            val_2 = str[i][3::]
+            returnArr.append({val:val_2, "token":f'{val}.'})
+        elif str[i][0] == "-" and str[i][1] != "-":
+            val =str[i][2::]
+            returnArr.append({1:val, "token":"-"})
+        elif str[i][0::] == "---":
+            val = str[i]
+            returnArr.append({3:val, "token":"---"})
+        elif str[i][0:2] == "![":
+            # print(str[i])
+            pass
+        elif str[i][0] == "[":
+            pass
+        elif str[i][0::2] == "   ":
+            pass
+    print(returnArr)  
+
+def handle_hashtags(str, count):
+    if str[0] == "#":
+        str = str[1::]
+        return handle_hashtags(str, count+1)
+    return {count: str, "token":"#"}
+
+    
+
+tokenize_main(
+    """
+# h1,~# # h2,~# ## h3,~# ### h4,~# #### h5,~# ##### h6,~1. one,~2. two,~3. three,~    1. test,~    2. test,~    3. test,~- one,~- two,~- three,~    - one,~    - two,~    - three,~---,~[title](https://www.johnodonnell.xyz),~![text](image.jpg)
+              """)
