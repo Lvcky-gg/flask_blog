322914fed17b3238f2283a35e126f923c46165f4
lvcky-gg <matadorcrab@gmail.com>
2024-01-26 16:10:38 -0500

    fixed the card

--- view/frontend/src/components/postCard/index.js
+++ view/frontend/src/components/postCard/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ body, title }) => {
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div class="w-9/12 rounded overflow-hidden shadow-lg m-auto">
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">{title}</div>
         <p class="text-gray-700 text-base">{`${body
